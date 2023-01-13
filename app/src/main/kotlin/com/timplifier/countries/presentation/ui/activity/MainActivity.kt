4bb64f9e88302e69c9107f6568a52fe36d4bd7f8
Clar1fy <timplifier@gmail.com>
2023-01-13 18:57:49 +0600

    feat(app/src/main/kotlin/): new package definition and di

---
+++ app/src/main/kotlin/com/timplifier/countries/presentation/ui/activity/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.timplifier.countries.presentation.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import com.timplifier.countries.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main)
